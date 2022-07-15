5d52d3dad3aaa9f690e4865d7184577416750aa4
github Kassy <github@shinbokutaro0220@gmail.com>
2022-07-15 10:13:44 +0900

    メモを編集する処理を作成しました

--- routes.rb
+++ routes.rb
@@ -20,7 +20,6 @@ post '/' do
   memo = Memo.new(params[:title], params[:content])
   File.open('memos.json') do |file|
     # メモ格納ファイルが空の場合は、空配列を用意する
-    # TODO: オプションにsymbolize_namesを指定する
     memo_array = JSON.parse(file.read, symbolize_names: true) || []
     memo_hash = {id: memo.id, title: memo.title, content: memo.content, created_at: memo.created_at, updated_at: memo.updated_at}
     memo_array << memo_hash
@@ -38,11 +37,23 @@ get '/memos/:memo_id' do
 end
 
 get '/memos/:memo_id/edit' do
-  # メモ編集ページを表示
+  memos = File.open('memos.json') { |file| JSON.parse(file.read, symbolize_names: true) }
+  @memo = memos.find { |memo| memo[:id] == params[:memo_id] }
+
+  erb :edit
 end
 
 patch '/memos/:memo_id' do
-  # メモ更新処理
+  memos = File.open('memos.json') { |file| JSON.parse(file.read, symbolize_names: true) }
+  memos.each do |memo|
+    next unless memo[:id] == params[:memo_id]
+    memo[:title] = params[:title]
+    memo[:content] = params[:content]
+    memo[:updated_at] = Time.now.strftime("%F %T")
+  end
+  File.open('memos.json', 'w') { |file| JSON.dump(memos, file) }
+
+  redirect to("/memos/#{params[:memo_id]}")
 end
 
 delete '/memos/:memo_id' do
