7d3f93e0983c3843bd42334bb3d6824f7c8897ef
github Kassy <github@shinbokutaro0220@gmail.com>
2022-07-23 18:53:08 +0900

    メモクラスのid属性は、必要無くなったので削除しました

--- memo.rb
+++ memo.rb
@@ -5,10 +5,9 @@ require 'securerandom'
 # メモのクラス
 class Memo
   attr_accessor :title, :content, :updated_at
-  attr_reader :id, :created_at
+  attr_reader :created_at
 
   def initialize(title, content)
-    @id = SecureRandom.uuid
     @title = title
     @content = content
     @created_at = Time.now.strftime('%F %T')
@@ -16,6 +15,6 @@ class Memo
   end
 
   def to_hash
-    { id: @id, title: @title, content: @content, created_at: @created_at, updated_at: @updated_at }
+    { title: @title, content: @content, created_at: @created_at, updated_at: @updated_at }
   end
 end
